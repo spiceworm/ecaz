75fde1474e5205b7d65b1bc100278b8ed21eb707
spiceworm <me@spiceworm.xyz>
2024-01-23 22:50:28 -0700

    Migrate shared env vars to .env file

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-VERSION=0.1.0
-
 docker compose build
 
-docker tag ecaz_xyz-app:latest registry.digitalocean.com/ecaz-xyz/app:${VERSION}
+source .env
 
-docker push registry.digitalocean.com/ecaz-xyz/app:${VERSION}
+docker push registry.digitalocean.com/ecaz-xyz/app:${TAG}
