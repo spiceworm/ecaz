7c39bf468b33177b241a6e2ecbda74b093d6c41b
spiceworm <me@spiceworm.xyz>
2024-01-23 22:50:28 -0700

    Reduce number of gunicorn processes in production
    It was starting 17 workers which caused the container
    to run out of memory.

--- app/gunicorn.py
+++ app/gunicorn.py
@@ -5,7 +5,7 @@ bind = "0.0.0.0:8081"
 chdir = "/app"
 
 if os.environ["PROD"] == "1":
-    workers = multiprocessing.cpu_count() * 2 + 1
+    workers = multiprocessing.cpu_count()  # * 2 + 1
 else:
     reload = True
     workers = 1
