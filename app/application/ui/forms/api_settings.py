b72d39cd120ba208dd0f98b7e09c8fc334fda6c8
spiceworm <me@spiceworm.xyz>
2024-01-23 22:50:28 -0700

    Add ability to create expiring tokens on api_settings page

--- app/application/ui/forms/api_settings.py
+++ app/application/ui/forms/api_settings.py
@@ -1,6 +1,8 @@
 import flask_wtf
 from wtforms import (
+    BooleanField,
     IntegerField,
+    SelectField,
     StringField,
     validators,
 )
@@ -13,6 +15,34 @@ __all__ = (
 
 
 class CreateApiTokenForm(flask_wtf.FlaskForm):
+    EXPIRES_NEVER = "Never"
+    EXPIRES_UNIT_MICROSECONDS = "Microseconds"
+    EXPIRES_UNIT_MILLISECONDS = "Milliseconds"
+    EXPIRES_UNIT_SECONDS = "Seconds"
+    EXPIRES_UNIT_MINUTES = "Minutes"
+    EXPIRES_UNIT_HOURS = "Hours"
+    EXPIRES_UNIT_DAYS = "Days"
+    EXPIRES_UNIT_WEEKS = "Weeks"
+
+    expires_number = IntegerField(
+        "expires_number",
+        render_kw={"disabled": True},
+        default=0,
+    )
+    expires_unit = SelectField(
+        "expires_unit",
+        choices=[
+            (EXPIRES_NEVER, EXPIRES_NEVER),
+            (EXPIRES_UNIT_MICROSECONDS, EXPIRES_UNIT_MICROSECONDS),
+            (EXPIRES_UNIT_MILLISECONDS, EXPIRES_UNIT_MILLISECONDS),
+            (EXPIRES_UNIT_SECONDS, EXPIRES_UNIT_SECONDS),
+            (EXPIRES_UNIT_MINUTES, EXPIRES_UNIT_MINUTES),
+            (EXPIRES_UNIT_HOURS, EXPIRES_UNIT_HOURS),
+            (EXPIRES_UNIT_DAYS, EXPIRES_UNIT_DAYS),
+            (EXPIRES_UNIT_WEEKS, EXPIRES_UNIT_WEEKS),
+        ],
+        default=EXPIRES_NEVER,
+    )
     token_name = StringField(
         "name",
         render_kw={"placeholder": "Name"},
