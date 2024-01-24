33e318042d427ddeb4b1bc69b4f1b1ddf408c813
spiceworm <me@spiceworm.xyz>
2024-01-23 22:50:28 -0700

    Change string storage from unicode to string type

--- app/application/models.py
+++ app/application/models.py
@@ -7,13 +7,13 @@ db = flask_sqlalchemy.SQLAlchemy()
 
 class ApiToken(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode)
-    value = db.Column(db.Unicode)
+    name = db.Column(db.String)
+    value = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 
 class User(db.Model, flask_login.UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Unicode)
+    username = db.Column(db.String)
     password_hash = db.Column(db.String)
     api_tokens = db.relationship("ApiToken", backref="user")
