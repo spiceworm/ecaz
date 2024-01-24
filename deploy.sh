fcc15b1c7d77a4f029b5ca1b69cae67d463f8a0a
spiceworm <me@spiceworm.xyz>
2024-01-23 22:50:28 -0700

    Add retry option to deploy.sh
    Running deploy.sh with -r will push the last image
    built for the given TAG. Useful for if uploading
    to an image registry did not succeed the first time.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,24 @@ set -e
 
 TAG=0.1.0
 
-docker compose build --no-cache
-docker tag registry.digitalocean.com/ecaz-xyz/app:latest registry.digitalocean.com/ecaz-xyz/app:${TAG}
-docker push registry.digitalocean.com/ecaz-xyz/app:${TAG}
+retry_flag=''
+
+print_usage() {
+    printf "Usage: ./deploy.sh [-r] \n"
+}
+
+while getopts 'r' flag; do
+    case "${flag}" in
+        r) retry_flag='true' ;;
+        *) print_usage
+            exit 1 ;;
+    esac
+done
+
+if [ "$retry_flag" = true ] ; then
+    docker push registry.digitalocean.com/ecaz-xyz/app:${TAG}
+else
+    docker compose build --no-cache
+    docker tag registry.digitalocean.com/ecaz-xyz/app:latest registry.digitalocean.com/ecaz-xyz/app:${TAG}
+    docker push registry.digitalocean.com/ecaz-xyz/app:${TAG}
+fi
