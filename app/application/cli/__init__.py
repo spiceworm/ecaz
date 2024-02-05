fe181274de30692f6bd853b97dfc1ae28924afd4
spiceworm <me@spiceworm.xyz>
2024-02-05 12:09:12 -0700

    Add generate-data cli command for development purposes

--- app/application/cli/__init__.py
+++ app/application/cli/__init__.py
@@ -1,4 +1,6 @@
+import random
 import re
+import secrets
 
 import click
 import flask
@@ -6,10 +8,16 @@ import flask_mailman
 
 from application.constants import messages
 from application.models import (
+    Comment,
     db,
+    Thread,
+    Topic,
     User,
 )
-from application.util.misc import csv_to_list
+from application.util.misc import (
+    csv_to_list,
+    generate_unique_username,
+)
 
 
 cli_bp = flask.Blueprint(
@@ -43,6 +51,57 @@ def get_endpoints():
     """
     for obj in flask.current_app.url_map.iter_rules():
         click.echo(f"{obj.endpoint} -> {obj.rule}")
+
+
+@cli_bp.cli.command("generate-data")  # pragma: no cover
+@click.option("-o", "--object-multiplier", count=True, default=1, show_default=True)
+@click.option("-w", "--wipe-db", is_flag=True, help="Reset database before generating data")
+def generate_data(object_multiplier, wipe_db):
+    """
+    This command only exists for development purposes
+    """
+    assert not flask.current_app.config["PROD"]
+
+    if wipe_db:
+        db.drop_all()
+        db.create_all()
+
+    topic_count = 5 * object_multiplier
+    with click.progressbar(range(topic_count), label=f"Creating {topic_count} topics") as count:
+        for i in count:
+            topic = Topic(name=f"Topic-{i}", description=f"Topic {i} description")
+            db.session.add(topic)
+            db.session.commit()
+
+    user_count = 100 * object_multiplier
+    topics = Topic.query.all()
+    with click.progressbar(range(user_count), label=f"Creating {user_count} users and a thread for each user") as count:
+        for _ in count:
+            username = generate_unique_username()
+            u = User(email=f"{username}@test.com", password=secrets.token_hex(), username=username)
+            db.session.add(u)
+            db.session.commit()
+            topic_idx = random.randint(0, len(topics) - 1)
+            topic = topics[topic_idx]
+            topic.create_thread(body=f"Body {u.username}", title=f"Title {u.username}", discussion=u.discussion)
+
+    comment_count = 5 * object_multiplier
+    threads = Thread.query.all()
+    with click.progressbar(User.query.all(), label=f"Creating {comment_count} comments for each user") as users:
+        for user in users:
+            for i in range(comment_count):
+                thread_idx = random.randint(0, len(threads) - 1)
+                thread = threads[thread_idx]
+                thread.create_comment(body=f"Comment {user.username}-{i}", discussion=user.discussion)
+
+    reply_count = 5 * object_multiplier
+    comments = Comment.query.all()
+    with click.progressbar(User.query.all(), label=f"Creating {reply_count} replies for each user") as users:
+        for user in users:
+            for i in range(reply_count):
+                comment_idx = random.randint(0, len(comments) - 1)
+                comment = comments[comment_idx]
+                comment.create_comment(body=f"Reply {user.username}-{i}", discussion=user.discussion)
 
 
 @cli_bp.cli.command("mark-admin")
