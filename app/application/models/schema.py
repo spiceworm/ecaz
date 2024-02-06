9e6a53bce112acece3e5138cee32ada754252dce
spiceworm <me@spiceworm.xyz>
2024-02-06 12:41:40 -0700

    Passing self to marshallow.Nested is deprecated

--- app/application/models/schema.py
+++ app/application/models/schema.py
@@ -62,7 +62,7 @@ class CommentSchema(marshmallow.SQLAlchemySchema):
     # If we are looking up a nested comment, there is no need to include the
     # `thread` attribute in the response for every other parent comment.
     # `thread` will already be included in the top level of the response json.
-    parent = marshmallow.Nested("self", exclude=("thread",))
+    parent = marshmallow.Nested(lambda: CommentSchema, exclude=("thread",))
     topic = marshmallow.Nested(TopicSchema)
     thread = marshmallow.Nested(ThreadSchema)
     unique_id = marshmallow.auto_field()
