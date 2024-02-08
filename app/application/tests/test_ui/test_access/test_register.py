673d23b6b9f88cce7ddf6106519228616a3f0c6f
spiceworm <me@spiceworm.xyz>
2024-02-07 19:49:13 -0700

    Organize test_register.py

--- app/application/tests/test_ui/test_access/test_register.py
+++ app/application/tests/test_ui/test_access/test_register.py
@@ -8,45 +8,44 @@ from application.models import (
 )
 
 
-def test_register(client):
-    """
-    Verify form submission to /register with valid values creates a `User` entry
-    and redirects the user to their profile page.
-    """
-    resp = client.post(
-        url_for("ui_bp.register"),
-        follow_redirects=True,
-        data={"email": "user@test.com", "password": "password123"},
-    )
-    user = User.query.filter_by(email="user@test.com")
-    assert user
-    assert len(resp.history) == 1
-    assert resp.request.base_url == url_for("ui_bp.profile")
+class TestRegister:
+    def test_valid(self, client):
+        """
+        Verify form submission to /register with valid values creates a `User` entry
+        and redirects the user to their profile page.
+        """
+        resp = client.post(
+            url_for("ui_bp.register"),
+            follow_redirects=True,
+            data={"email": "user@test.com", "password": "password123"},
+        )
+        user = User.query.filter_by(email="user@test.com")
+        assert user
+        assert len(resp.history) == 1
+        assert resp.request.base_url == url_for("ui_bp.profile")
 
+    def test_if_already_authenticated(self, ui_user):
+        """
+        Verify an authenticated user trying to access /register is redirected to their
+        profile page.
+        """
+        resp = ui_user().get(
+            url_for("ui_bp.register"),
+            follow_redirects=True,
+        )
+        assert len(resp.history) == 1
+        assert resp.request.base_url == url_for("ui_bp.profile")
 
-def test_register_attempt_if_authenticated(ui_user):
-    """
-    Verify an authenticated user trying to access /register is redirected to their
-    profile page.
-    """
-    resp = ui_user().get(
-        url_for("ui_bp.register"),
-        follow_redirects=True,
-    )
-    assert len(resp.history) == 1
-    assert resp.request.base_url == url_for("ui_bp.profile")
-
-
-def test_register_duplicate_email(client, user):
-    """
-    Verify that the correct error message is shown if a user tries to register
-    using an email address that is already in use by another user.
-    """
-    u = user()
-    resp = client.post(
-        url_for("ui_bp.register"),
-        follow_redirects=True,
-        data={"email": u.email, "password": "some-password"},
-    )
-    assert messages.DUPLICATE_EMAIL_ERROR in resp.data.decode()
-    db.session.rollback()
+    def test_if_duplicate_email(self, client, user):
+        """
+        Verify that the correct error message is shown if a user tries to register
+        using an email address that is already in use by another user.
+        """
+        u = user()
+        resp = client.post(
+            url_for("ui_bp.register"),
+            follow_redirects=True,
+            data={"email": u.email, "password": "some-password"},
+        )
+        assert messages.DUPLICATE_EMAIL_ERROR in resp.data.decode()
+        db.session.rollback()
